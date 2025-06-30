6bdb706ca6d99c51e38ccb994d33262d4c107d4e
Ramdas Ghorpade <150921308+Ghorpaderamdas@users.noreply.github.com>
2025-07-01 01:03:54 +0530

    Updated CampBuddyButton.tsx

--- src/components/CampBuddyButton.tsx
+++ src/components/CampBuddyButton.tsx
@@ -21,7 +21,7 @@ const CampBuddyButton = () => {
   return (
     <>
       {/* Main Floating Button */}
-      <<div className="fixed bottom-16 sm:bottom-20 md:bottom-24 right-14 z-50">
+      <div className="fixed bottom-16 sm:bottom-20 md:bottom-24 right-14 z-50">
 
         <motion.div
           className="relative"
